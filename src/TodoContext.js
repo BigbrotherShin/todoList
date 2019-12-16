1a0276fede4a5e2844e3e3867ff2ef3f513c15ae
BigbrotherShin <shinjhhp5@gmail.com>
2019-12-16 18:25:32 +0900

    todoList

---
+++ src/TodoContext.js
@@ -0,0 +1,89 @@
+import React, { useReducer, createContext, useContext, useRef } from 'react';
+
+const initialTodos = [
+  {
+    id: 1,
+    text: '프로젝트 생성하기',
+    done: true,
+  },
+  {
+    id: 2,
+    text: '컴포넌트 스타일링하기',
+    done:true,
+  },
+  {
+    id: 3,
+    text: 'Context 만들기',
+    done: false,
+  },
+  {
+    id: 4,
+    text: '기능 구현하기',
+    done: false,
+  }
+];
+
+function todoReducer(state, action) {
+  switch (action.type) {
+    case 'CREATE':
+      return state.concat(action.todo);
+    case 'TOGGLE':
+      return state.map(todo => 
+        todo.id === action.id ? {...todo, done: !todo.done } : todo
+      );
+    case 'REMOVE':
+      return state.filter(todo => todo.id !== action.id);
+    default:
+      throw new Error(`Unhandled action type: ${action.type}`);
+  };
+}
+
+const TodoStateContext = createContext();
+const TodoDispatchContext = createContext();
+const TodoNextIdContext = createContext();
+
+// Context 를 만들면, Context 안에 Provider 라는 컴포넌트가 들어있는데 이 컴포넌트를 통하여 Context 의 값을 정할 수 있습니다. 이 컴포넌트를 사용할 때, value 라는 값을 설정해주면 됩니다.
+
+// <UserDispatch.Provider value={dispatch}>...</UserDispatch.Provider>
+// 이렇게 설정해주고 나면 Provider 에 의하여 감싸진 컴포넌트 중 어디서든지 우리가 Context 의 값을 다른 곳에서 바로 조회해서 사용 할 수 있습니다
+
+export function TodoProvider({ children }) {
+  const [state, dispatch] = useReducer(todoReducer, initialTodos);
+  const nextId = useRef(5);
+
+  return (
+    <TodoStateContext.Provider value={state}>
+      <TodoDispatchContext.Provider value={dispatch}>
+        <TodoNextIdContext.Provider value={nextId}>
+          {children}
+        </TodoNextIdContext.Provider>
+      </TodoDispatchContext.Provider>
+    </TodoStateContext.Provider>
+  );
+}
+
+// 컴포넌트에서 useContext 를 직접 사용하는 대신에, useContext 를 사용하는 커스텀 Hook 을 만들어서 내보내주겠습니다.
+
+export function useTodoState() {
+  const context = useContext(TodoStateContext);
+  if (!context) {
+    throw new Error ('Cannot find TodoProvider');
+  }
+  return context;
+}
+
+export function useTodoDispatch() {
+  const context = useContext(TodoDispatchContext);
+  if (!context) {
+    throw new Error('Cannot find TodoProvider');
+  }
+  return context;
+}
+
+export function useTodoNextId() {
+  const context = useContext(TodoNextIdContext);
+  if (!context) {
+    throw new Error('Cannot find TodoProvider');
+  }
+  return context;
+}
